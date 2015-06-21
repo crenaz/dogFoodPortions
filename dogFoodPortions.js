9f3fdb76d490c66600e946e99cb1cfc40d9e1eab
crenaz <crenaz@gmail.com>
2015-06-21 15:08:38 -0400

    third edit, with the .js file!

---
+++ dogFoodPortions.js
@@ -0,0 +1,12 @@
+console.log("Hello, Welcome to the DogFood Portion Trasitioner App!"); 
+console.log("  Please enter the amount of days to transition:");
+
+var transitionDays = Number(prompt("Pick a number of days","")), start=0, tree="#";
+ if(!isNaN(transitionDays))
+  while (transitionDays>start) {
+  console.log(tree);
+  start+=1;
+  tree=tree+"#";
+}
+ else alert("Hey, Why didn't you give me a number?");
+
